55f6246894a9769ad4c84a9a435a585e67d2dd02
rafaelnunes34 <rafael.nunes34@gmail.com>
2022-02-07 19:51:48 -0300

    projeto criado

---
+++ src/main/java/com/example/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.example.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.entities.Product;
+import com.example.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> findAll() {
+		return productRepository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = productRepository.findById(id);
+		return obj.get();
+	}
+}
---
+++ src/test/java/com/example/course/Course1ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.course;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course1ApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
